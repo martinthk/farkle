d2affd07bb54c02438c6d7bbfec58c6792f36a1f
Martin Tom <73326875+martinthk@users.noreply.github.com>
2020-11-05 12:13:16 +0800

    Add files via upload

---
+++ Menu.h
@@ -0,0 +1,16 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+class Menu
+{
+public:
+	static void showRules();
+	static void showMenu();
+};
+#endif // !MENU_H
+
